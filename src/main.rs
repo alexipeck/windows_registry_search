1778776ba9646176d642b2ae66c9cae0e3d6a29b
Alexi Peck <alexinicolaspeck@gmail.com>
2023-11-10 01:18:44 +1100

    Switched to tokio for async, use number of threads available on the CPU, there is no real benefit going above this with the async methodology

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use parking_lot::Mutex;
+use tokio::sync::Notify;
 use std::{
-    collections::VecDeque,
+    collections::{VecDeque, HashSet},
     error::Error,
     sync::{
         atomic::{AtomicUsize, Ordering},
@@ -11,93 +12,97 @@ use std::{
 };
 use winreg::{enums::*, RegKey};
 
-const MAX_THREADS: usize = 64;
-
 static KEY_COUNT: AtomicUsize = AtomicUsize::new(0);
 static VALUE_COUNT: AtomicUsize = AtomicUsize::new(0);
 static HKLM: RegKey = RegKey::predef(HKEY_LOCAL_MACHINE);
 
-fn feed_queue_and_process_values(
-    key_path: &str,
-    worker_manager: &Arc<WorkerManager>,
-) -> Result<(), Box<dyn Error>> {
-    if worker_manager.string_matches(key_path) {
-        println!("{}", key_path);
+pub struct WorkerManager {
+    threads: usize,
+    search_terms: Vec<String>,
+    key_queue: Arc<Mutex<VecDeque<String>>>,
+    work_ready_for_processing: Arc<Notify>,
+    threads_waiting_for_work: Arc<AtomicUsize>,
+    no_work_left: Arc<Notify>,
+    pub results: Arc<Mutex<HashSet<String>>>,
+}
+
+impl WorkerManager {
+    pub fn new(search_terms: Vec<String>, threads_to_use: usize) -> Self {
+        Self {
+            threads: threads_to_use,
+            search_terms: search_terms.into_iter().map(|term| term.to_lowercase()).collect(),
+            key_queue: Arc::new(Mutex::new(VecDeque::new())),
+            work_ready_for_processing: Arc::new(Notify::new()),
+            threads_waiting_for_work: Arc::new(AtomicUsize::new(0)),
+
+            no_work_left: Arc::new(Notify::new()),
+
+            results: Arc::new(Mutex::new(HashSet::new())),
+        }
     }
-    let registry_key = HKLM.open_subkey_with_flags(key_path, KEY_READ)?;
-    {
-        let mut key_paths = Vec::new();
-        for key_result in registry_key.enum_keys() {
-            KEY_COUNT.fetch_add(1, Ordering::SeqCst);
-            match key_result {
-                Ok(key_name) => {
-                    let key_path = format!("{}\\{}", key_path, key_name);
-                    key_paths.push(key_path);
+
+    fn feed_queue_and_process_values(
+        &self,
+        key_path: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        if self.string_matches(key_path) {
+            self.results.lock().insert(key_path.to_string());
+        }
+        let registry_key = HKLM.open_subkey_with_flags(key_path, KEY_READ)?;
+        {
+            let mut key_paths = Vec::new();
+            for key_result in registry_key.enum_keys() {
+                KEY_COUNT.fetch_add(1, Ordering::SeqCst);
+                match key_result {
+                    Ok(key_name) => {
+                        let key_path = format!("{}\\{}", key_path, key_name);
+                        key_paths.push(key_path);
+                    }
+                    Err(_err) => {
+                        //println!("{}", err)
+                    },
+                }
+            }
+            self.feed_queue(key_paths);
+            self.work_ready_for_processing.notify_waiters();
+        }
+    
+        for value_result in registry_key.enum_values() {
+            VALUE_COUNT.fetch_add(1, Ordering::SeqCst);
+            match value_result {
+                Ok((value_name, reg_value)) => {
+                    let data = reg_value.to_string();
+                    if self.any_string_matches(&value_name, &data) {
+                        self.results.lock().insert(format!(
+                            "Name: {}, Type: {:?}, Data: \"{}\"",
+                            value_name,
+                            reg_value.vtype,
+                            data
+                        ));
+                    }
                 }
                 Err(_err) => {
                     //println!("{}", err)
                 },
             }
         }
-        worker_manager.feed_queue(key_paths);
+        Ok(())
     }
 
-    for value_result in registry_key.enum_values() {
-        VALUE_COUNT.fetch_add(1, Ordering::SeqCst);
-        match value_result {
-            Ok((value_name, reg_value)) => {
-                let data = reg_value.to_string();
-                if worker_manager.any_string_matches(&value_name, &data) {
-                    format!(
-                        "Name: {}, Type: {:?}, Data: \"{}\"",
-                        value_name,
-                        reg_value.vtype,
-                        data
-                    );
-                }
-            }
-            Err(_err) => {
-                //println!("{}", err)
-            },
-        }
-    }
-    Ok(())
-}
-
-pub struct WorkerManager {
-    max_threads: usize,
-    current_threads: AtomicUsize,
-    timeout: Duration,
-    search_terms: Vec<String>,
-    key_queue: Arc<Mutex<VecDeque<String>>>,
-}
-
-impl WorkerManager {
-    pub fn new(max_threads: usize, timeout: u64, search_terms: Vec<String>) -> Self {
-        Self {
-            max_threads,
-            current_threads: AtomicUsize::new(0),
-            timeout: Duration::from_millis(timeout),
-            search_terms: search_terms.into_iter().map(|term| term.to_lowercase()).collect(),
-            key_queue: Arc::new(Mutex::new(VecDeque::new())),
-        }
-    }
-    fn wait(&self) {
-        while self.current_threads.load(Ordering::SeqCst) > self.max_threads {
-            thread::sleep(self.timeout)
-        }
-    }
-    pub fn acquire(&self) -> String {
+    pub async fn get_work(&self) -> Option<String> {
         loop {
-            self.wait();
-            if let Some(key) = self.key_queue.lock().pop_front() {
-                self.current_threads.fetch_add(1, Ordering::SeqCst);
-                return key;
-            }
+            let work = self.key_queue.lock().pop_front();
+            if let Some(key) = work {
+                return Some(key);
+            } else {
+                self.threads_waiting_for_work.fetch_add(1, Ordering::SeqCst);
+                tokio::select! {
+                    _ = self.work_ready_for_processing.notified() => {},
+                    _ = self.no_work_left.notified() => return None,
+                }
+                self.threads_waiting_for_work.fetch_sub(1, Ordering::SeqCst);
+            }  
         }
-    }
-    pub fn release(&self) {
-        self.current_threads.fetch_sub(1, Ordering::SeqCst);
     }
 
     pub fn feed_queue(&self, keys: Vec<String>) {
@@ -126,37 +131,50 @@ impl WorkerManager {
         false
     }
 
-    pub fn output_progress_until_finish(&self) {
-        let start_time = Instant::now();
-        let min_runtime = Duration::from_secs(1);
+    pub async fn run(&self, worker_manager: Arc<WorkerManager>) {
+        for _ in 0..worker_manager.threads {
+            let worker_manager = worker_manager.to_owned();
+            tokio::spawn(run_thread(worker_manager));
+        }
+        self.work_ready_for_processing.notify_waiters();
         loop {
-            if self.key_queue.lock().len() == 0 && start_time.elapsed() > min_runtime {
-                break;
+            if worker_manager.threads_waiting_for_work.load(Ordering::SeqCst) == worker_manager.threads {
+                if self.key_queue.lock().len() == 0 {
+                    self.no_work_left.notify_waiters();
+                    break;
+                } else {
+                    self.work_ready_for_processing.notify_waiters();
+                }
             }
-            println!("Key count: {}, Value count: {}", KEY_COUNT.load(Ordering::SeqCst), VALUE_COUNT.load(Ordering::SeqCst));
-            thread::sleep(Duration::from_secs(1));
+            thread::sleep(Duration::from_millis(100));
         }
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let worker_manager = Arc::new(WorkerManager::new(MAX_THREADS, 500, vec!["Google Chrome".to_string(), "7-Zip".to_string()]));
+async fn run_thread(worker_manager: Arc<WorkerManager>) {
+    loop {
+        let key_path = match worker_manager.get_work().await {
+            Some(key_path) => key_path,
+            None => break,
+        };
+        if let Err(_err) = worker_manager.feed_queue_and_process_values(&key_path) {
+            //println!("{}", err);
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let worker_manager = Arc::new(WorkerManager::new(vec!["Google Chrome".to_string(), "7-Zip".to_string()], num_cpus::get()));
 
     worker_manager.feed_queue(vec!["Software".to_string()]);
     let start_time = Instant::now();
-    for _ in 0..MAX_THREADS {
-        let worker_manager = worker_manager.to_owned();
+    worker_manager.run(worker_manager.to_owned()).await;
 
-        thread::spawn(move || loop {
-            let worker_manager = worker_manager.to_owned();
-            let key_path = worker_manager.acquire();
-            if let Err(_err) = feed_queue_and_process_values(&key_path, &worker_manager) {
-                //println!("{}", err);
-            }
-            worker_manager.release();
-        });
+    for result in worker_manager.results.lock().iter() {
+        println!("{}", result);
     }
-    worker_manager.output_progress_until_finish();
+    println!("Key count: {}, Value count: {}", KEY_COUNT.load(Ordering::SeqCst), VALUE_COUNT.load(Ordering::SeqCst));
     println!("Completed in {}ms.", start_time.elapsed().as_millis());
     Ok(())
 }
